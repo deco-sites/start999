89058a8d0fb518a0b573ceb3a1001c082e6d165c
Erick Mairink <contact.emairink@gmail.com>
2024-06-24 05:58:37 +0000

    Release staging environment with 1 changes made by Erick Mairink

--- sections/on.tsx
+++ sections/on.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'preact/hooks';
+
 
 interface Props {
   /**
@@ -7,25 +7,9 @@ interface Props {
   name?: string;
 }
 
-const MigrationBanner = ({ file } : Props) => {
-  console.log(file)
-  useEffect(() => {
-    const style = document.createElement('style');
-    style.textContent = `
-      .background-arrows {
-        background: url("https://t3.ftcdn.net/jpg/00/86/56/12/360_F_86561234_8HJdzg2iBlPap18K38mbyetKfdw1oNrm.jpg") no-repeat center;
-        background-size: cover;
-      }
-    `;
-    document.head.append(style);
-    return () => {
-      document.head.removeChild(style);
-    };
-  }, []);
-
-  
+const MigrationBanner = ({ file } : Props) => {  
   return (
-    <div className="background-arrows flex flex-col items-center justify-center text-center h-screen bg-blue-900 text-white">
+    <div className="background-arrows bg-[url('https://t3.ftcdn.net/jpg/00/86/56/12/360_F_86561234_8HJdzg2iBlPap18K38mbyetKfdw1oNrm.jpg')] flex flex-col items-center justify-center text-center h-screen bg-blue-900 text-white">
       <img src={file} />
       <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold mb-4">
         UMA MIGRAÇÃO VTEX IO DESCOMPLICADA,
